4c920c970785536ea2b56fb463601b14d83a29b4
PhoenixTAN <tanziqi1756@outlook.com>
2020-11-16 22:23:47 -0500

    Assignment 8 CUDA Programming backup.

---
+++ Assignments/8-CUDA-Programming/README.md
@@ -0,0 +1,28 @@
+# Assignment 8 CUDA Programming 
+Group 9
+
+Ziqi Tan, U88387934, ziqi1756@bu.edu
+
+Xueyan Xia, U82450191, xueyanx@bu.edu
+
+## Collaboration
+Pair discussing, designing, programming and debugging.
+
+## Compile instructions
+```
+$ nvcc -o two_sum two_sum.cu
+$ ./two_sum
+```
+
+## Result
+```
+Running sequential version: 
+Time: 3.05472
+Running cuda version: 
+Time: 1.99212
+True
+speedup: 1.5334
+efficiency: 1.42809e-09
+```
+
+
--- Assignments/8-CUDA-Programming/two_sum.cu
+++ Assignments/8-CUDA-Programming/two_sum.cu
@@ -1,189 +1,159 @@
 #include <iostream>
 #include <chrono>       /* time manipulation */
-#include <iomanip>      /* float number precission */
 
 const int LENGTH{1 << 30};
-const int MAX_VAL{1 << 5};
+const int MAX_VAL{1 << 4};
 
 /**
- * @description: add up vector a and vector b with CUDA
- * @param {float*} a: matrix A 
- * @param {float*} b: matrix B
- * @param {float*} result: the result of vector addition
- * @param {int} length: the length of the vector
+ * @description: kernel definition
+ * @param {float*} x: vector x
+ * @param {float*} y: vector y
+ * @param {float*} z: vector z, the addition of x and y
+ * @param {int} length: the length of vectors
  */
-__global__ void add_vectors(float* a, float* b, float* result, int length) {
+__global__ void add_vectors(float* x, float* y, float* z, int length) {
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if ( tid < length ) {
-        result[tid] = a[tid] + b[tid];
+        z[tid] = x[tid] + y[tid];
     }
 
 }
 
 /**
- * @description: add up vector a and vector b with CUDA
- * @param {float*} a: matrix A 
- * @param {float*} b: matrix B
- * @param {float*} result: the result of vector addition
- * @param {float*} result_from_cuda: the result of vector addition
- * @param {int} length: the length of the vector
+ * @description: initialize vectors
+ * @param {float*} x: vector x
+ * @param {float*} y: vector y
+ * @param {float*} z: vector z
+ * @param {float*} z_from_cuda: vector z_from_cuda
+ * @param {int} length: the length of vectors
  */
-void initialize(float* a, float* b, float* result, float* result_from_cuda, int length) {
+void initialize(float* x, float* y, float* z, float* z_from_cuda, int length) {
     srand(1);
     for ( int i = 0; i < length; i++ ) {
-        a[i] = rand() % MAX_VAL / 100.0;
-        b[i] = rand() % MAX_VAL / 100.0;
-        result[i] = 0;
-        result_from_cuda[i] = 0;
+        x[i] = rand() % MAX_VAL;
+        y[i] = rand() % MAX_VAL;
+        z[i] = 0;
+        z_from_cuda[i] = 0;
     }
 }
 
 /**
- * @description: sequential version of adding up vector a and vector b
- * @param {float*} a: matrix A 
- * @param {float*} b: matrix B
- * @param {float*} result: the result of vector addition
- * @param {int} length: the length of the vector
+ * @description: sequential version of vector addition
+ * @param {float*} x: vector x
+ * @param {float*} y: vector y
+ * @param {float*} z: vector z
+ * @param {int} length: the length of vectors
  */
-void sequential_add_vectors(float* a, float* b, float* result, int length) {
+void sequential_add_vectors(float* x, float* y, float* z, int length) {
     for ( int i = 0; i < length; i++ ) {
-        result[i] = a[i] + b[i];
+        z[i] = x[i] + y[i];
     }
 }
 
-
-bool validate_result(float* a, float* b, int length) {
-    
-    std::cout << "Validating result ... " << std::endl;
-
+/**
+ * @description: validate results from sequential version and parallel version
+ * @param {float*} h_z: vector h_z
+ * @param {float*} h_z_from_cuda: vector h_z_from_cuda
+ * @param {int} length: the length of vectors
+ */
+void validate_result(float* h_z, float* h_z_from_cuda, int length){
     for ( int i = 0; i < length; i++ ) {
-        if ( a[i] != b[i] ) {
-            std::cout << "false" << std::endl;
-            return false;
+        if ( h_z_from_cuda[i] != h_z[i] ){
+            std::cout << "False" << std::endl;
+            return;
         }
     }
     
-    std::cout << "true" << std::endl;
-    return true;
+    std::cout << "True" << std::endl;
 }
 
 
 int main() {
-
-    // create local data objects
-    float* a = new float[LENGTH];
-    float* b = new float[LENGTH];
-    float* result = new float[LENGTH];
-    float* result_from_cuda = new float[LENGTH];
-
-    initialize(a, b, result, result_from_cuda, LENGTH);
-
-    // time manipulation
+    
+    std::chrono::steady_clock::time_point start_time_cuda;
+    std::chrono::steady_clock::time_point end_time_cuda;
+    std::chrono::duration<double> duration_cuda;
     std::chrono::steady_clock::time_point start_time;
     std::chrono::steady_clock::time_point end_time;
-    std::chrono::steady_clock::time_point preprocess_end_time;
-    std::chrono::steady_clock::time_point cleanup_end_time;
     std::chrono::duration<double> duration;
-    std::chrono::duration<double> preprocess_time;
-    std::chrono::duration<double> cleanup_time;
-
     double t1;
     double tp;
-    double t_preprocess;
-    double t_cleanup;
 
-    // sequential version
-    std::cout << "============================================" << std::endl;
-    std::cout << "Running sequential version: " << std::endl;
-    start_time = std::chrono::steady_clock::now();
-    sequential_add_vectors(a, b, result, LENGTH);
-    end_time = std::chrono::steady_clock::now();
-    duration = end_time - start_time;
-    t1 = duration.count();
-
-    std::cout << std::setprecision(2) << "it takes " << t1 << " seconds." << std::fixed << std::scientific << std::endl;
-    std::cout << "============================================" << std::endl;
-
-    // cuda version
-    const int threadsPerBlock = 256;
-    const int blocksPerGrid = (LENGTH + threadsPerBlock - 1) / threadsPerBlock;
-    float* cuda_a;
-    float* cuda_b;
-    float* cuda_result;
-
-    std::cout << "Running cuda version: " << std::endl;
-    start_time = std::chrono::steady_clock::now();
-
-    // set the ID of the CUDA device
+     // set the ID of the CUDA device
     cudaSetDevice(0);
 
-    // Allocate vectors in device memory
-    cudaMalloc(&cuda_a, sizeof(float) * LENGTH);
-    cudaMalloc(&cuda_b, sizeof(float) * LENGTH);
-    cudaMalloc(&cuda_result, sizeof(float) * LENGTH);
+    // create local data objects
+    float* h_x = new float[LENGTH];
+    float* h_y = new float[LENGTH];
+    float* h_z = new float[LENGTH];
+    float* h_z_from_cuda = new float[LENGTH];
 
-    // copy vectors from host memory to device memory
-    cudaMemcpy(cuda_a, a, sizeof(float) * LENGTH, cudaMemcpyHostToDevice);
-    cudaMemcpy(cuda_b, b, sizeof(float) * LENGTH, cudaMemcpyHostToDevice);
-    cudaMemcpy(cuda_result, result_from_cuda, sizeof(float) * LENGTH, cudaMemcpyHostToDevice);
-    preprocess_end_time = std::chrono::steady_clock::now();
-    
-    // invoke kernel
-    add_vectors<<<blocksPerGrid, threadsPerBlock>>>(cuda_a, cuda_b, cuda_result, LENGTH);
-    // copy result from device memory to host memeoy
-    cudaMemcpy(result_from_cuda, cuda_result, sizeof(float) * LENGTH, cudaMemcpyDeviceToHost);
+    initialize(h_x, h_y, h_z, h_z_from_cuda, LENGTH);
+
+    std::cout << "Running sequential version: " << std::endl;
+    start_time = std::chrono::steady_clock::now();
+
+    sequential_add_vectors(h_x, h_y, h_z, LENGTH);
 
     end_time = std::chrono::steady_clock::now();
-    duration = end_time - preprocess_end_time;
+    duration = end_time - start_time; 
+    t1 = duration.count();
+    std::cout << "Time: " << t1 << std::endl;
+   
+    // cuda version
+    const int threadsPerBlock = 1024;
+    const int numOfBlocks = (LENGTH + threadsPerBlock - 1) / threadsPerBlock;
 
-    // Free device memory
-    cudaFree(cuda_a);
-    cudaFree(cuda_b);
-    cudaFree(cuda_result);
-    cleanup_end_time = std::chrono::steady_clock::now();
+    size_t size = LENGTH * sizeof(float);
 
+    float* d_x;
+    float* d_y;
+    float* d_z;
+
+    cudaMalloc(&d_x, size);
+    cudaMalloc(&d_y, size);
+    cudaMalloc(&d_z, size);
+
+    
+    std::cout << "Running cuda version: " << std::endl;
+    start_time_cuda = std::chrono::steady_clock::now();
+
+    // Copy vectors from host memory to device memory
+    cudaMemcpy(d_x, h_x, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, h_y, size, cudaMemcpyHostToDevice);
+
+    // cuda operation
+    add_vectors<<<numOfBlocks, threadsPerBlock>>>(d_x, d_y, d_z, LENGTH);
+
+    // Copy result from device memory to host memory
+    cudaMemcpy(h_z_from_cuda, d_z, size, cudaMemcpyDeviceToHost);
+
+    end_time_cuda = std::chrono::steady_clock::now();
+    duration_cuda = end_time_cuda - start_time_cuda;
+    tp = duration_cuda.count();
+    std::cout << "Time: " << tp << std::endl;
+
+    // free device memory
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_z);
+    
     // synchronize the GPU preventing premature termination
     cudaDeviceSynchronize();
 
-    tp = duration.count();
-    preprocess_time = preprocess_end_time - start_time;
-    t_preprocess = preprocess_time.count();
-    cleanup_time = cleanup_end_time - end_time;
-    t_cleanup = cleanup_time.count();
+    validate_result(h_z, h_z_from_cuda, LENGTH);
 
-    std::cout << "it takes " << std::setprecision(2)  << tp << " seconds." << std::scientific << std::endl;
-    std::cout << "preprocess time: " << std::setprecision(2)  << t_preprocess  << std::scientific << " seconds." << std::endl;
-    std::cout << "cleanup_time: " << std::setprecision(2)  << t_cleanup << std::scientific << " seconds." << std::endl;
-    std::cout << "total: " << std::setprecision(2)  << tp + t_preprocess + t_cleanup << std::scientific << " seconds." << std::endl;
-    std::cout << "============================================" << std::endl;
+    // free host memory
+    delete[] h_z_from_cuda;
+    delete[] h_z;
+    delete[] h_x;
+    delete[] h_y;
 
-    // validating result
-    validate_result(result, result_from_cuda, LENGTH);
-
-    // cleanup host memory
-    delete[] a;
-    delete[] b;
-    delete[] result;
-    delete[] result_from_cuda;
-
-    std::cout << "============================================" << std::endl;
-
-    // calculate speed up
     double speedup = t1 / tp;
-    std::cout << "speedup: " << speedup << std::scientific << std::endl;
-
-    // efficiency
-    double efficiency = speedup / blocksPerGrid / threadsPerBlock;
-    std::cout << "efficiency: " << efficiency << std::scientific << std::endl;
-
-    std::cout << "============================================" << std::endl;
-    std::cout << "if we take the preprocess time and the cleanup time as a cost of cuda computation" << std::endl;
-    speedup = t1 / (tp + t_preprocess + t_cleanup);
-    std::cout << "speedup: " << speedup << std::scientific << std::endl;
-    efficiency = speedup / blocksPerGrid / threadsPerBlock;
-    std::cout << "efficiency: " << efficiency << std::scientific << std::endl;
-    std::cout << "============================================" << std::endl;
+    double efficiency = speedup / (numOfBlocks * threadsPerBlock);
+    std::cout << "speedup: " << speedup << std::endl;
+    std::cout << "efficiency: " << efficiency << std::endl;
 
     return 0;
 }
